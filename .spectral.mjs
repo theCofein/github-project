4a8fca413f70c4f93b86583b5e40b0d5dee5bdd3
vbohacek@monetplus.cz <vbohacek@monetplus.cz>
2025-11-21 10:53:03 +0100

    Added .spectral.mjs

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjI5OTIyOQ/spectral.js?branch_slug=main&token=50df8910-5574-4084-92db-d3042b0166e4";
+export default { extends: ruleset };
